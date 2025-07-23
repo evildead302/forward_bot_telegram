717c879e62c91a19a475738442782750695e86a9
evildead302 <jonepeter672@gmail.com>
2025-07-23 12:22:21 +0500

    Update forward.py

--- forward.py
+++ forward.py
@@ -25,7 +25,7 @@ class ForwardBot:
         self.MAX_PARALLEL = 1
         self.SCAN_WORKERS = 5
         self.SCAN_BATCH_SIZE = 5000
-        self.FORWARD_DELAY = 60
+        self.FORWARD_DELAY = 5
         self.PROGRESS_UPDATE_INTERVAL = 2
         self.GET_HISTORY_LIMIT = 1000
 
